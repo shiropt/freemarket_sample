156a25ac654fa141edfc8f2077e015fe563c74a2
shiropt <63380875+shiropt@users.noreply.github.com>
2020-05-11 12:00:44 +0900

    itemモデル作成

--- db/migrate/20200507100448_create_items.rb
+++ db/migrate/20200507100448_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string  :name, null: false
+      t.string  :size
+      t.string  :condition, null: false
+      t.text  :description, null: false
+      t.boolean  :shipping_fee_side, null: false
+      t.integer  :price, null: false
+      t.integer  :shipping_days
+      t.integer  :user_id, null: false, null: false
+      t.integer  :brand_id, null: false, null: false
+      t.integer  :category_id, null: false, null: false
+      t.integer  :prefectures_id, null: false, null: false
       t.timestamps
     end
   end
