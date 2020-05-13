3aa4a03f001a41f2155c93c956879e3ff06ecb67
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-13 09:35:39 +0900

    トップ商品表示修正
    売り切れの商品は表示させないようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:user, :category, :brand, :purchased_info).limit(3).order("created_at DESC")
+    @items = Item.includes(:images, :purchased_info).limit(3).order("created_at DESC")
   end
 end
