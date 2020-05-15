166abd9cf27d7748c314d08eacd1adb92b2152db
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-15 09:28:27 +0900

    itemモデル修正
    itemモデルにprefectureのactive_hashを利用するためextendを記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   validates :name, :condition,:description,:shipping_fee_side,:price, presence: true
   
   belongs_to :user
-  # belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prefecture
   belongs_to :brand
   belongs_to :category
   has_one :purchased_info
