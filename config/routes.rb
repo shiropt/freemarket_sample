efda4f760d18a42869943e86539016cbe2bcead2
shiropt <63380875+shiropt@users.noreply.github.com>
2020-05-17 12:35:34 +0900

    商品購入コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'buyers/index'
+  get 'buyers/done'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :items, only:[:index, :show]
   resources :category, only:[:index,:show]
