7e195a2e78f9abde3129dda1914af56142dccbf1
shiropt <63380875+shiropt@users.noreply.github.com>
2020-05-17 19:22:45 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-
   get 'buyers/index'
   get 'buyers/done'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+ 
+  devise_for :users,
+     controllers: { registrations: 'users/registrations',
+                    sessions: 'users/sessions' }
   resources :items, only:[:index, :show]
   resources :category, only:[:index,:show]
-  resources :buyers, only:[:show]
+  resources :buyers, only:[:show] do
+    collection do
+      get 'done'
+    end
+  end
   root 'items#index'
 
 end
