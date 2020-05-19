9f12cefdbe77f077d89abf52040ee682e9bd3ad1
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-19 19:04:21 +0900

    トップページview修正
    view崩れ修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
                     sessions: 'users/sessions' }
   resources :items
   resources :category, only:[:index,:show]
-  root 'items#new'
+  root 'items#index'
 
 end
 
