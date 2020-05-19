3f28f605648da5e2cd02dac145f9f29b5f825f82
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-19 16:17:10 +0900

    view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: [:show]
   resources :items, only:[:index, :show]
   resources :category, only:[:index,:show]
   root 'items#index'
