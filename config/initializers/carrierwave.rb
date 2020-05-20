a3ff37d2b9e2f669d94afc446f4987d0b5fd52aa
hammmmm <61265522+hammmmm@users.noreply.github.com>
2020-05-20 14:54:21 +0900

    carrierwave修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],#credentails.ymlで鍵を保管
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],#credentails.ymlで鍵を保管
+      aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'furima-74g'
