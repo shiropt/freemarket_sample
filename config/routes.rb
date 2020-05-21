a14f42a6ced881460997667928b8f8a4a467b84b
shiropt <63380875+shiropt@users.noreply.github.com>
2020-05-22 07:15:16 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: { registrations: 'users/registrations',
     sessions: 'users/sessions' }
-    resources :items, except: [:edit, :update, :destroy]
+    resources :items
     resources :category, only:[:index,:show]
     resources :users, only: [:show]
     resources :buyers, only:[:show]
