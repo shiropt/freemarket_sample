e07c9668cc2ca07c47a7c5e65ce05f1e248d82f9
OLiVE-wumf-1126 <63233287+OLiVE-wumf-1126@users.noreply.github.com>
2020-05-23 21:29:22 +0900

    不必要ファイル、sider修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,23 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'users/registrations',
     sessions: 'users/sessions' }
-  resources :items, only:[:index, :new, :create, :show, :destroy] do
-    collection do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
-    resources :buyers, only:[:index] do
+    resources :items, except: [:edit, :update] do
       collection do
-        get 'done', to: 'buyers#done'
-        post 'buy', to: 'buyers#buy'
+        get 'get_category_children', defaults: { format: 'json' }
+        get 'get_category_grandchildren', defaults: { format: 'json' }
+      end
+      resources :buyers, only:[:index] do
+        collection do
+          get 'done', to: 'buyers#done'
+          post 'buy', to: 'buyers#buy'
+        end
       end
     end
-  end
-  resources :category, only:[:index,:show]
-  resources :users, only: [:show]
+    resources :category, only:[:index,:show]
+    resources :users, only: [:show]
 
   root 'items#index'
-  resources :cards, only:[:index, :new, :create, :destroy]
+  resources :cards, only:[:index, :new, :create, :show, :destroy]
 
 end
 
