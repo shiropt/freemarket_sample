ca87d9a4e64ba12b7636ebbae142b1ed2c2b2fb6
shiropt <63380875+shiropt@users.noreply.github.com>
2020-05-24 19:07:22 +0900

    initialcommit

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,11 +2,23 @@ module ItemsHelper
   # 送料負担を定義
   def shipping_fee_side
     fee = @item.shipping_fee_side
-  if fee ===true
+    if fee ===true
       "送料込み"
     else
       "着払い（購入者負担）"
     end
   end
+  
+  
+  # サイズが未入力の場合の条件分岐を定義
+  def size
+   if @item.size === nil
+    "指定なし"
+   else
+    @item.size.name
+
+   end
+  end
+
 end
       
